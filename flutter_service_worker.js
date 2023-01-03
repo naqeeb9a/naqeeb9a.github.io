1eb17f8a17c4e8fd17c63e20fec86f7f77614c3f
naqeeb9a <76917886+naqeeb9a@users.noreply.github.com>
2023-01-03 11:47:44 +0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "9df29c5459fe704ea7d4432dd24a10ce",
+  "assets/AssetManifest.json": "feb3497c8c0436e2f9721541e9c9a574",
 "assets/assets/emptyList.json": "83854d616f55bd22ca63c91a17226f6d",
 "assets/assets/loader.json": "1fff2f186cbdb39c0478402db9c3f8a8",
-"assets/assets/nonVeg.png": "9511a6c5ef55f753f5447f0117efc9b0",
 "assets/assets/resturant.json": "095408858bd861c56a576bc33e16eef5",
 "assets/assets/splash.png": "f87ddb45c903f935d5480958cbb45b86",
 "assets/assets/upload_logo.jpg": "da68b9173f083294c97a5e5ebab9e61a",
-"assets/assets/veg.png": "dbddf6adf71b9dd8f6b0deaa7574d4bf",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/loading.gif": "0438ecb3741b901931d63a26bdbf69eb",
@@ -38,9 +36,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "478956e966c686bbf09a0467c2de4c4a",
 "icons/Icon-maskable-192.png": "291598ff76dd6af8c89b1c3035917b26",
 "icons/Icon-maskable-512.png": "478956e966c686bbf09a0467c2de4c4a",
-"index.html": "7949bb34d91b8d72b02d600f5421e9f7",
-"/": "7949bb34d91b8d72b02d600f5421e9f7",
-"main.dart.js": "86779dd412e5aef8c7181fdad592e0c6",
+"index.html": "76018a1ed62ede1ed70075d3a344e197",
+"/": "76018a1ed62ede1ed70075d3a344e197",
+"main.dart.js": "eafd7de6d7dbe3f0df927c25ae5ec4a6",
 "manifest.json": "8a0d8d610887ef62685b34f5fb9cb653",
 "version.json": "399c2a5a175995698167491d7f08a99a"
 };
